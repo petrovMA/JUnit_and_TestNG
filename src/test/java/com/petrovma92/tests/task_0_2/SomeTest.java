aff93375de9d5232d4f73c17766f3b897867b8a7
user <petrovma92@gmail.com>
2017-06-14 22:04:34 +0300

    Initial commit

---
+++ src/test/java/com/petrovma92/tests/task_0_2/SomeTest.java
@@ -0,0 +1,17 @@
+package com.petrovma92.tests.task_0_2;
+
+import org.testng.annotations.Test;
+
+
+public class SomeTest {
+
+    @Test
+    public void test1() {
+        System.out.println("com.petrovma92.tests.task_0_2.SomeTest.test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("com.petrovma92.tests.task_0_2.SomeTest.test2");
+    }
+}
